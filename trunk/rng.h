b3503596524a2465ab9071f8c732a43214957ba5
lorens <lorens@863fe2fa-e75c-5ebc-2b10-f6c81824c9e2>
2004-01-25 12:41:35 +0000

    Added support for dynamic creation of random number generators.

--- trunk/rng.h
+++ trunk/rng.h
@@ -1,7 +1,7 @@
 /*
  * Interface to MT19937 random number generator.
  *
- * Copyright (C) 2003 Carnegie Mellon University
+ * Copyright (C) 2003, 2004 Carnegie Mellon University
  *
  * This file is part of Ymer.
  *
@@ -19,7 +19,7 @@
  * along with Ymer; if not, write to the Free Software Foundation,
  * Inc., #59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  *
- * $Id: rng.h,v 1.1 2003-11-12 03:58:48 lorens Exp $
+ * $Id: rng.h,v 2.1 2004-01-25 12:41:35 lorens Exp $
  */
 #ifndef RNG_H
 #define RNG_H
@@ -28,7 +28,15 @@
 extern "C" {
 #endif
 
-/* Initializes state[N] with the given seed. */
+typedef struct {
+  unsigned long aaa;
+  unsigned long maskB, maskC;
+  unsigned long *state;
+  int left, initf;
+  unsigned long *next;
+} mt_struct;
+
+/* Seeds the MT19937 random number generator. */
 void init_genrand(unsigned long s);
 
 /* Generates an integer random number between 0 and 0xffffffff (both
@@ -43,6 +51,31 @@ double genrand_real2(void);
 
 /* Generates a random number in the interval (0,1). */
 double genrand_real3(void);
+
+/* Construct id-specific MT521 random number generator. */
+mt_struct *get_mt_parameter_id(unsigned short id);
+
+/* Deletes the given id-specific random number generator. */
+void free_mt_struct(mt_struct *mts);
+
+/* Seeds the given id-specific random number generator. */
+void init_genrand_id(unsigned long s, mt_struct *mts);
+
+/* Generates an integer random number between 0 and 0xffffffff (both
+   inclusive) using the given id-specific generator. */
+unsigned long genrand_int32_id(mt_struct *mts);
+
+/* Generates a random number in the interval [0,1] using the given
+   id-specific generator. */
+double genrand_real1_id(mt_struct *mts);
+
+/* Generates a random number in the interval [0,1) using the given
+   id-specific generator. */
+double genrand_real2_id(mt_struct *mts);
+
+/* Generates a random number in the interval (0,1) using the given
+   id-specific generator. */
+double genrand_real3_id(mt_struct *mts);
 
 #ifdef	__cplusplus
 }
