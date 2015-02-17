6dbe8b8c6cbd1d47d7c94a18b776579a5c5cdab9
hlsyounes@gmail.com <hlsyounes@gmail.com@863fe2fa-e75c-5ebc-2b10-f6c81824c9e2>
2015-02-17 20:15:23 +0000

    Add parser support for init state expressions.  This change makes it possible to use Ymer with PRISM model files that contain init state expressions. Such expressions are still ignored by the tool, but at least their presence no longer results in parse errors.

--- trunk/parser.hh
+++ trunk/parser.hh
@@ -176,7 +176,7 @@ extern int yydebug;
 typedef union YYSTYPE YYSTYPE;
 union YYSTYPE
 {
-#line 454 "parser.yy" /* yacc.c:1909  */
+#line 456 "parser.yy" /* yacc.c:1909  */
 
   Type type;
   const std::string* str;
