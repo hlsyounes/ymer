81a23279996b10052f0f68f5110c60f48fab7890
Haakan Younes <hlsyounes@gmail.com>
2019-01-14 16:46:19 -0500

    Make sure non-zero exit codes from Ymer register as failures in ymer_regtest.sh.

--- src/ymer_regtest.sh
+++ src/ymer_regtest.sh
@@ -19,6 +19,8 @@
 # along with Ymer; if not, write to the Free Software Foundation,
 # Inc., #59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
+set -o pipefail
+
 pass=1
 
 readonly YMER=${YMER:-./ymer}
